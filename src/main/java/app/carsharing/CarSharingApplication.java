72d06e39aeb92b10df03b85368d067f36b75a1cf
Olexandr Fetisov <149307126+fetis-off@users.noreply.github.com>
2024-11-25 22:06:04 +0200

    added checkstyle.xml, application.properties and configure ci.yaml

--- src/main/java/app/carsharing/CarSharingApplication.java
+++ src/main/java/app/carsharing/CarSharingApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CarSharingApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(CarSharingApplication.class, args);
     }
-
 }
